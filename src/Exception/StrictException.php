bb8826332f9b79c5def2b593d5debef3b5241c99
achsoft <acfatah@gmail.com>
2014-07-08 18:01:28 +0800

    Unnecessary spaces

--- src/Exception/StrictException.php
+++ src/Exception/StrictException.php
@@ -4,7 +4,7 @@
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE file.
  * Redistributions of files must retain the above copyright notice.
- * 
+ *
  * @copyright (c) 2014, Achmad F. Ibrahim
  * @link https://github.com/Achsoft
  * @license http://opensource.org/licenses/mit-license.php The MIT License (MIT)
@@ -24,5 +24,5 @@ use Achsoft\Component\ErrorHandler\Exception\ErrorException;
  */
 class StrictException extends ErrorException
 {
-    
+
 }
